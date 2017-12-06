9ffbbb870632dbccf8a4feff0ca938cb2b589aa0
ichte <xt2810@gmail.com>
2017-12-06 23:47:28 +0700

    u

---
+++ src/Auth/AuthService.php
@@ -0,0 +1,29 @@
+<?php
+namespace Ichte\XtUser\Auth;
+
+use Zend\Authentication\AuthenticationService;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+class AuthService extends AuthenticationService
+{
+    public function __invoke(ServiceLocatorInterface $container)
+    {
+        $credentialValidationCallback = function($dbCredential, $requestCredential) {
+            $bcrypt =new \Zend\Crypt\Password\Bcrypt();
+            $bcrypt->setCost(14);
+            return $bcrypt->verify($requestCredential,$dbCredential);
+        };
+
+        $this->authAdapter = new CallbackCheckAdapter(
+            \Ahdbase\Common::getdatabase()->Adapter,
+            'user',
+            null,
+            'password',
+            $credentialValidationCallback
+        );
+
+        $this->setStorage(new Session('AHDCOMPANY'));
+        $this->setAdapter($this->authAdapter);
+        return $this;
+    }
+}
